5aa0f5b42095d10c43bc494570f565b1f3634e2e
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-18 18:04:03 +0200

    python_variable_annotations

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Takes a list of strings and returns a tuple of the string and its length."""
-from typing import Iterable, Sequence, Tuple
+from typing import Iterable, Sequence, Tuple, List
 
 
-def element_length(lst: Iterable[Sequence]) -> Tuple[Sequence, int]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Takes a list of strings"""
     return [(i, len(i)) for i in lst]
