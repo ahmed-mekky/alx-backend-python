bc2a2df6f0211e489bc156b6eaf0da1c7822319f
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-18 17:15:24 +0200

    python_variable_annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from typing import List
+def sum_list(input_list: List[float]) -> float:
+    sum = 0
+    for item in input_list:
+        sum += item
+    return sum
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from typing import Union
+def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+    sum = 0
+    for item in mxd_lst:
+        sum += item
+    return sum
