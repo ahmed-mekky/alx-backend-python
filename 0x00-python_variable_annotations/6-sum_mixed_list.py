aab8dda51d3170f20ec7b285ccbd184eb490abf0
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-18 17:33:06 +0200

    Update sum_list and sum_mixed_list functions in python_variable_annotations

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-from typing import Union
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     sum = 0
     for item in mxd_lst:
         sum += item
