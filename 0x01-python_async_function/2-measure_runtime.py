67074b4ea30db8f6fa12dbecd3e55fcdcc4646a1
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-23 04:58:47 +0200

    python_async_function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """importance of async and await syntax in python async function"""
 import asyncio
+import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Measure the runtime of the wait_n function"""
-    start = asyncio.get_event_loop().time()
-    await wait_n(n, max_delay)
-    end = asyncio.get_event_loop().time()
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.perf_counter()
     return float((end - start) / n)
