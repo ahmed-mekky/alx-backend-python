dd0cfaf2a0eadc4bbfcac5e2d89749fc74b6b762
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-23 03:54:44 +0200

    python_async_function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """importance of async and await syntax in python async function"""
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
-    """async function"""
-    delays = [wait_random(max_delay) for i in range(n)]
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Async function that returns a list of sorted delay times"""
+    delays = [wait_random(max_delay) for _ in range(n)]
     results = await asyncio.gather(*delays)
     return sorted(results)
