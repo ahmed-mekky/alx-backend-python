15e6c00d54de21a63899b09daaec972cdc16794b
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-23 03:38:01 +0200

    python_async_function

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""importance of async and await syntax in python async function"""
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """async function"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
