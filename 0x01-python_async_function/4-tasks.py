6bc55cf171d1919e41b399f751391851f5eed8c4
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-23 04:37:04 +0200

    python_async_function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """importance of async and await syntax in python async function"""
 import asyncio
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks.py').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def task_wait_n(n: int, max_delay: int) -> float:
     """async function"""
-    delays = [wait_random(max_delay) for i in range(n)]
+    delays = [task_wait_random(max_delay) for i in range(n)]
     return [await delay for delay in asyncio.as_completed(delays)]
