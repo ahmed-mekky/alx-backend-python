23c103f800b4998381f478f358fc71963a97599b
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-24 03:20:59 +0200

    0x02-python_async_comprehension

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """importance of async and await syntax in python async function"""
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+async_com = __import__('1-async_com').async_com
 import asyncio
 import time
 
@@ -8,6 +8,6 @@ import time
 async def measure_runtime() -> float:
     """measure_runtime"""
     start = time.perf_counter()
-    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    await asyncio.gather(async_com(), async_com(), async_com(), async_com())
     end = time.perf_counter()
     return float(end - start)
